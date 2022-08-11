392e1390e17236ac109283c6800edc1976f7d840
vasotona-cen69602 <vasasotona@seznam.cz>
2022-08-11 09:51:07 +0200

    added counter

--- app/src/main/java/com/learningpackage/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/learningpackage/myfirstapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.learningpackage.myfirstapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,8 +11,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnClickMe = findViewById<Button>(R.id.mybutton)
+        val tvMytextView = findViewById<TextView>(R.id.textView)
+        var timesClicked = 0
         btnClickMe.setOnClickListener {
-            btnClickMe.text = "you clicked me"
+
+            timesClicked = timesClicked + 1
+            tvMytextView.text = timesClicked.toString()
+
         }
     }
 }
